8ffaa5bba4f1b79a9a1e4c13837fd2d7262fd93b
ciphersujan <146098451+Sujan4002@users.noreply.github.com>
2024-10-18 21:19:32 +0530

    done

---
+++ src/components/dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../others/Header'
+
+const EmployeeDashboard = () => {
+  return (
+    <div className='p-10 bg-[#1c1c1c] h-screen'>
+      <Header/>
+    </div>
+  )
+}
+
+export default EmployeeDashboard
