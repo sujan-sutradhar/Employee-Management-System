577dc6412927a361ff17efa18b21ff1d0a9242f3
ciphersujan <146098451+Sujan4002@users.noreply.github.com>
2024-10-19 23:09:32 +0530

    employee dashboard succesfully done

--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -3,14 +3,60 @@ import React from 'react'
 const TaskList = () => {
   return (
     <div id="tasklist"className='overflow-x-auto h-[55%] w-full py-5 flex items-center justify-start gap-5 flex-nowrap mt-10'>
-<div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl '></div>
-<div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl '></div>
-<div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl '></div>
-<div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl '></div>
-<div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl '></div>
-<div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl '></div>
-
- 
+<div className='flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl  p-5'>
+  <div className='flex justify-between items-center '>
+     <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+     <h4 className='text-sm'>19 Oct 2024</h4>
+  </div>
+  <h2 className='mt-5 text-2xl font-semibold'>Make a cover song</h2>
+  <p className='text-sm mt-1.5'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aut, ab sequi corrupti architecto possimus.
+  </p>
+</div>
+<div className='flex-shrink-0 h-full w-[300px] bg-blue-400 rounded-xl  p-5'>
+  <div className='flex justify-between items-center '>
+     <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+     <h4 className='text-sm'>19 Oct 2024</h4>
+  </div>
+  <h2 className='mt-5 text-2xl font-semibold'>Make a cover song</h2>
+  <p className='text-sm mt-1.5'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aut, ab sequi corrupti architecto possimus.
+  </p>
+</div>
+<div className='flex-shrink-0 h-full w-[300px] bg-zinc-400 rounded-xl  p-5'>
+  <div className='flex justify-between items-center '>
+     <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+     <h4 className='text-sm'>19 Oct 2024</h4>
+  </div>
+  <h2 className='mt-5 text-2xl font-semibold'>Make a cover song</h2>
+  <p className='text-sm mt-1.5'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aut, ab sequi corrupti architecto possimus.
+  </p>
+</div>
+<div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl  p-5'>
+  <div className='flex justify-between items-center '>
+     <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+     <h4 className='text-sm'>19 Oct 2024</h4>
+  </div>
+  <h2 className='mt-5 text-2xl font-semibold'>Make a cover song</h2>
+  <p className='text-sm mt-1.5'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aut, ab sequi corrupti architecto possimus.
+  </p>
+</div>
+<div className='flex-shrink-0 h-full w-[300px] bg-orange-400 rounded-xl  p-5'>
+  <div className='flex justify-between items-center '>
+     <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+     <h4 className='text-sm'>19 Oct 2024</h4>
+  </div>
+  <h2 className='mt-5 text-2xl font-semibold'>Make a cover song</h2>
+  <p className='text-sm mt-1.5'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aut, ab sequi corrupti architecto possimus.
+  </p>
+</div>
+<div className='flex-shrink-0 h-full w-[300px] bg-pink-400 rounded-xl  p-5'>
+  <div className='flex justify-between items-center '>
+     <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+     <h4 className='text-sm'>19 Oct 2024</h4>
+  </div>
+  <h2 className='mt-5 text-2xl font-semibold'>Make a cover song</h2>
+  <p className='text-sm mt-1.5'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aut, ab sequi corrupti architecto possimus.
+  </p>
+</div>
     </div>
     
   )
