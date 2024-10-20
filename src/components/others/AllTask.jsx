79f7f91fc2f68570e002f876e86dca07b74267c5
ciphersujan <146098451+Sujan4002@users.noreply.github.com>
2024-10-20 20:47:12 +0530

    Full admin ui is ready

---
+++ src/components/others/AllTask.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] h-48 rounded mt-6 overflow-auto'>
+    <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2>Sujan Sutradhar</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+        </div>
+        <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2>Sujan Sutradhar</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+        </div>
+        <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2>Sujan Sutradhar</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+        </div>
+        <div className='bg-yellow-400  mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2>Sujan Sutradhar</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+        </div>
+        <div className='bg-blue-400  mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2>Sujan Sutradhar</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+        </div>
+        <div className='bg-pink-400  mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2>Sujan Sutradhar</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+        </div>
+        <div className='bg-purple-400  mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2>Sujan Sutradhar</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+        </div>
+    </div>
+  )
+}
+
+export default AllTask
