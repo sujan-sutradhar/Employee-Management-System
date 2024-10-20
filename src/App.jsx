04dc8e69438878602ef4ccec6305b2298ffc93ee
ciphersujan <146098451+Sujan4002@users.noreply.github.com>
2024-10-20 20:18:58 +0530

    new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Login from './components/auth/Login'
 import EmployeeDashboard from './components/dashboard/EmployeeDashboard'
+import AdminDashboard from './components/dashboard/AdminDashboard'
 const App = () => {
   return (
     <>
    {/* <Login/> */}
-   <EmployeeDashboard/>
+   {/* <EmployeeDashboard/> */}
+   <AdminDashboard/>
     </>
   )
 }
